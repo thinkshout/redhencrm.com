e17e4f377b263420bc2190326e2177092f77f10d
Brett Heckman <brett@thinkshout.com>
2013-04-04 21:09:56 -0700

    Adding nav toggle

---
+++ javascripts/redhen.js
@@ -0,0 +1,18 @@
+$(function() {
+	
+  $('#toggle a').bind('click', function() {
+  	$('ul.primary-menu').slideToggle('400');
+    event.preventDefault();
+  });
+
+  var ww = document.body.clientWidth;
+
+  if (ww < 800) {
+  	$('ul.primary-menu a').bind('click', function() {
+  		$('ul.primary-menu').slideToggle('400');
+  	});
+  } else {
+
+  }
+  
+});
